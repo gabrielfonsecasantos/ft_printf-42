b21e272a69611331763cb2756628f6e233d518d7
Gabriel Fonseca <gfonseca882@gmail.com>
2026-06-23 12:30:23 -0300

    Adding logic to convert %i, %d and %u // erased code to convert %f, because it's not necessary

--- ft_printf.c
+++ ft_printf.c
@@ -12,7 +12,7 @@ int ft_strlen(char  *s)
   return (counter);
 }
 
-void  ft_putnbr(int n)
+void  ft_putnbr(double n)
 {
   long  num;
 
@@ -28,36 +28,12 @@ void  ft_putnbr(int n)
   write(1, &num, 1);
 }
 
-void decimalConversion(float deciPart)
+void  ft_putnbr_unsigned(unsigned int num)
 {
-  int precision;
-  char digit;
-  int numDigit;
-
-  precision = 0;
-  while (precision < 6)
-  {
-    deciPart *= 10;
-    numDigit = (int)deciPart;
-    digit = numDigit + '0';
-    write(1, &digit, 1);
-    precision++; 
-    deciPart -= numDigit;  
-  }
-}
-
-void  ft_putfloat(float n)
-{
-  long  intPart;
-  float  deciPart;
-  long  temp;
-
-  temp = n / 1;
-  intPart = temp;
-  deciPart = (n - temp);
-  ft_putnbr(intPart);
-  write(1, ".", 1);
-  decimalConversion(deciPart);
+  if (num >= 10)
+    ft_putnbr(num / 10);
+  num = (num % 10) + '0';
+  write(1, &num, 1);
 }
 
 int ft_printf(const char *str, ...)
@@ -65,7 +41,7 @@ int ft_printf(const char *str, ...)
   va_list list;
   char  c;
   char  *s;
-  float f;
+  double f;
 
   va_start(list, str);
   while(*str)
@@ -86,14 +62,11 @@ int ft_printf(const char *str, ...)
         write(1, s, ft_strlen(s));
       }
       // checks if the conversion is to an int
-      if (*str == 'i')
-        ft_putnbr(va_arg(list, int)); 
-      // checks if the conversion is to a float
+      if (*str == 'i' || *str == 'd')
+        ft_putnbr(va_arg(list, int));
+       // checks if the conversion is to an unsigned int
       if (*str == 'u')
-      {
-        f = va_arg(list, double);
-        ft_putfloat(f);
-      } 
+        ft_putnbr_unsigned(va_arg(list, unsigned int));
       // checks if is necessary to print a % sign
       if (*str == '%')
         write (1, "%", 1);
@@ -111,8 +84,10 @@ int ft_printf(const char *str, ...)
 
 int main(void)
 {
-  ft_printf("%s, %c, %i, %u.", "alou", 'a', 2147483647, 0.005);
+  unsigned int a = 3000000000;
+
+  ft_printf("%s, %c, %i, %u.", "alou", 'a', -214748364, a);
   printf("\n");
-  printf("%s, %c, %i, %f.", "alou", 'a', 2147483647, 0.005);
+  printf("%s, %c, %i, %u.", "alou", 'a', -214748364, a);
   return (0);
 }
