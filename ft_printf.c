799fb8874b843950ccfa9655c1f9dae8d476a35d
Gabriel Fonseca <gfonseca882@gmail.com>
2026-06-23 13:01:47 -0300

    Correctin aux functions: helper functions and main function now return the correct number os characters printed

--- ft_printf.c
+++ ft_printf.c
@@ -1,93 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/23 12:31:52 by gabriel           #+#    #+#             */
+/*   Updated: 2026/06/23 12:58:04 by gabriel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
-#include <stdio.h>
 #include <limits.h>
+#include <stdio.h>
 
-int ft_strlen(char  *s)
+int	ft_strlen(char *s)
 {
-  int counter;
+	int	counter;
 
-  counter = 0;
-  while (s[counter])
-    counter++;
-  return (counter);
+	counter = 0;
+	while (s[counter])
+		counter++;
+	return (counter);
 }
 
-void  ft_putnbr(double n)
+int	ft_putnbr(int n)
 {
-  long  num;
+	long	num;
+	int		len;
 
-  num = n;
-  if (num < 0)
-  {
-    num *= -1;
-    write(1, "-", 1);
-  }
-  if (num >= 10)
-    ft_putnbr(num / 10);
-  num = (num % 10) + '0';
-  write(1, &num, 1);
+	num = n;
+	len = 0;
+	if (num < 0)
+	{
+		num *= -1;
+		len += write(1, "-", 1);
+	}
+	if (num >= 10)
+		len += ft_putnbr(num / 10);
+	num = (num % 10) + '0';
+	len += write(1, &num, 1);
+	return (len);
 }
 
-void  ft_putnbr_unsigned(unsigned int num)
+int	ft_putnbr_unsigned(unsigned int num)
 {
-  if (num >= 10)
-    ft_putnbr(num / 10);
-  num = (num % 10) + '0';
-  write(1, &num, 1);
+	int	len;
+
+	len = 0;
+	if (num >= 10)
+		len += ft_putnbr_unsigned(num / 10);
+	num = (num % 10) + '0';
+	len += write(1, &num, 1);
+	return (len);
 }
 
-int ft_printf(const char *str, ...)
+int	ft_printf(const char *str, ...)
 {
-  va_list list;
-  char  c;
-  char  *s;
-  double f;
+	va_list	list;
+	char	c;
+	char	*s;
+	int		counter;
 
-  va_start(list, str);
-  while(*str)
-  {
-    if (*str == '%')
-    {
-      str++;
-      // checks if the conversion is to char
-      if (*str == 'c')
-      {
-        c = va_arg(list, int);
-        write(1, &c, 1);
-      }
-      //cheks if the conversion is to string
-      if (*str == 's')
-      {
-        s = va_arg(list, char *);
-        write(1, s, ft_strlen(s));
-      }
-      // checks if the conversion is to an int
-      if (*str == 'i' || *str == 'd')
-        ft_putnbr(va_arg(list, int));
-       // checks if the conversion is to an unsigned int
-      if (*str == 'u')
-        ft_putnbr_unsigned(va_arg(list, unsigned int));
-      // checks if is necessary to print a % sign
-      if (*str == '%')
-        write (1, "%", 1);
-      str++;
-    } else 
-    {
-      c = *str;
-      write(1, &c, 1);
-      str++;
-    }
-  }
-  va_end(list);
-  return (0);
+	counter = 0;
+	va_start(list, str);
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			// checks if the conversion is to char
+			if (*str == 'c')
+			{
+				c = va_arg(list, int);
+				counter += write(1, &c, 1);
+			}
+			// cheks if the conversion is to string
+			if (*str == 's')
+			{
+				s = va_arg(list, char *);
+				if (!s)
+					s = "(null)";
+				counter += write(1, s, ft_strlen(s));
+			}
+			// checks if the conversion is to an int
+			if (*str == 'i' || *str == 'd')
+				counter += ft_putnbr(va_arg(list, int));
+			// checks if the conversion is to an unsigned int
+			if (*str == 'u')
+				counter += ft_putnbr_unsigned(va_arg(list, unsigned int));
+			// checks if is necessary to print a % sign
+			if (*str == '%')
+				counter += write(1, "%", 1);
+			str++;
+		}
+		else
+		{
+			c = *str;
+			counter += write(1, &c, 1);
+			str++;
+		}
+	}
+	va_end(list);
+	return (counter);
 }
 
-int main(void)
+/*int	main(void)
 {
-  unsigned int a = 3000000000;
+	unsigned int	a;
 
-  ft_printf("%s, %c, %i, %u.", "alou", 'a', -214748364, a);
-  printf("\n");
-  printf("%s, %c, %i, %u.", "alou", 'a', -214748364, a);
-  return (0);
+	a = 3000000000;
+	ft_printf("%s, %c, %i, %u.", "alou", '0', -214748364, a);
+	printf("\n");
+	printf("%s, %c, %i, %u.", "alou", 'a', -214748364, a);
+	return (0);
 }
+*/
--- ft_printf.h
+++ ft_printf.h
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/23 12:32:54 by gabriel           #+#    #+#             */
+/*   Updated: 2026/06/23 12:32:57 by gabriel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
-# define FT_PRINTF_H  
+# define FT_PRINTF_H
 
 # include <stdarg.h>
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
 
-int ft_printf(const char *str, ...);
+int	ft_printf(const char *str, ...);
 
 #endif
