879adf4073a8fcc08b6f3a9fe4e00710b8910972
Gabriel Fonseca <gfonseca882@gmail.com>
2026-06-24 16:21:56 -0300

    Adding %p logic and another file for helper functions

--- ft_printf.c
+++ ft_printf.c
@@ -6,63 +6,33 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 12:31:52 by gabriel           #+#    #+#             */
-/*   Updated: 2026/06/24 13:38:16 by gabriel          ###   ########.fr       */
+/*   Updated: 2026/06/24 16:20:09 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-int			ft_strlen(char *s);
-int			ft_putnbr(int n);
-int			ft_putstr(char *str);
-int			ft_putchar(char c);
-
-/*static int	ft_putptr(void *ptr)
-{
-	return (0);
-}
-*/
-
-static int	ft_putnbr_unsigned(unsigned int num)
-{
-	int	len;
-
-	len = 0;
-	if (num >= 10)
-		len += ft_putnbr_unsigned(num / 10);
-	num = (num % 10) + '0';
-	len += write(1, &num, 1);
-	return (len);
-}
-
-static int	ft_putnbr_hex(unsigned int n, int u)
-{
-	int		len;
-	char	c;
-
-	len = 0;
-	if (n >= 16)
-		len += ft_putnbr_hex(n / 16, u);
-	c = n % 16;
-	if (c < 10)
-		c = c + '0';
-	else if (u == 1)
-		c = c - 10 + 'A';
-	else
-		c = c - 10 + 'a';
-	len += write(1, &c, 1);
-	return (len);
-}
+int	ft_strlen(char *s);
+int	ft_putnbr(int n);
+int	ft_putstr(char *str);
+int	ft_putchar(char c);
+int	ft_putptr_hex(unsigned long num);
+int	ft_putptr(void *ptr);
+int	ft_putnbr_unsigned(unsigned int num);
+int	ft_putnbr_hex(unsigned int n, int u);
 
 int	ft_formatter(char *str, va_list list)
 {
 	int	ret;
 
+	ret = 0;
 	if (*str == 'c')
 		ret = ft_putchar(va_arg(list, int));
 	if (*str == 's')
 		ret = ft_putstr(va_arg(list, char *));
+	if (*str == 'p')
+		ret = ft_putptr(va_arg(list, void *));
 	if (*str == 'i' || *str == 'd')
 		ret = ft_putnbr(va_arg(list, int));
 	if (*str == 'u')
@@ -83,27 +53,23 @@ int	ft_printf(const char *str, ...)
 	int		ret;
 
 	counter = 0;
-	ret = 0;
 	va_start(list, str);
 	while (*str)
 	{
 		if (*str == '%')
-		{
-			ret += ft_formatter((char *)++str, list);
-			if (ret == -1)
-				return (-1);
-			str++;
-		}
+			ret = ft_formatter((char *)++str, list);
 		else
+			ret = ft_putchar(*str);
+		if (ret == -1)
 		{
-			ret += ft_putchar(*str);
-			if (ret == -1)
-				return (-1);
-			str++;
+			va_end(list);
+			return (-1);
 		}
+		counter += ret;
+		str++;
 	}
 	va_end(list);
-	return (counter + ret);
+	return (counter);
 }
 
 /*int	main(void)
---
+++ ft_put_functions.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_put_functions.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/24 15:59:55 by gabriel           #+#    #+#             */
+/*   Updated: 2026/06/24 16:19:19 by gabriel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+
+int	ft_putnbr_hex(unsigned int n, int u)
+{
+	int		len;
+	char	c;
+
+	len = 0;
+	if (n >= 16)
+		len += ft_putnbr_hex(n / 16, u);
+	c = n % 16;
+	if (c < 10)
+		c = c + '0';
+	else if (u == 1)
+		c = c - 10 + 'A';
+	else
+		c = c - 10 + 'a';
+	len += write(1, &c, 1);
+	return (len);
+}
+
+int	ft_putptr_hex(unsigned long num)
+{
+	int		len;
+	char	c;
+
+	len = 0;
+	if (num >= 16)
+		len += ft_putptr_hex(num / 16);
+	c = num % 16;
+	if (c < 10)
+		c = c + '0';
+	else
+		c = c - 10 + 'a';
+	len += ft_putchar(c);
+	return (len);
+}
+
+int	ft_putptr(void *ptr)
+{
+	int				len;
+	unsigned long	addr;
+
+	if (!ptr)
+		return (ft_putstr("(nil)"));
+	addr = (unsigned long)ptr;
+	len = ft_putstr("0x");
+	len += ft_putptr_hex(addr);
+	return (len);
+}
+
+int	ft_putnbr_unsigned(unsigned int num)
+{
+	int	len;
+
+	len = 0;
+	if (num >= 10)
+		len += ft_putnbr_unsigned(num / 10);
+	num = (num % 10) + '0';
+	len += write(1, &num, 1);
+	return (len);
+}
